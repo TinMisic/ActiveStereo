f71161e23caaa32ffc12d1329b1695f2987340fb
Tin Misic <misic.tin@gmail.com>
2023-03-16 20:02:08 +0100

    Servo calibration and movement

---
+++ Code/servo_controller.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# offsets and constants definition
+scaling = 180/270
+offset = 135
+offsets = [x+offset for x in [2, 7, -4, -4]]
+
+def getAngles(cent_L, cent_R, CM_L, CM_R):
+    cL = (CM_L[0,2], CM_L[1,2]) # get center point for L
+    cR = (CM_R[0,2], CM_R[1,2]) # get center point for R
+
+    fxL = CM_L[0,0]
+    fyL = CM_L[1,1]
+
+    fxR = CM_R[0,0]
+    fyR = CM_R[1,1]
+
+    # calculation for left
+    uL = cL[0] - cent_L[0]
+    vL = -(cL[1] - cent_L[1])  # minus because of servo orientation -> negative vals for up, positive for down
+
+    alphaHL = np.rad2deg(np.arctan2(uL, fxL))
+    alphaVL = np.rad2deg(np.arctan2(vL, fyL))
+
+    # calculation for right
+    uR = cR[0] - cent_R[0]
+    vR = cR[1] - cent_R[1]  # servo orientation here is fine. negative for down, positive for up
+
+    alphaHR = np.rad2deg(np.arctan2(uR, fxR))
+    alphaVR = np.rad2deg(np.arctan2(vR, fyR))
+
+    # equalise vertical angles for both cameras to ensure center projection lines are coplanar
+    alphaV = (alphaVL + alphaVR) / 2
+
+    return ((alphaHL, alphaV),(alphaHR, alphaV))
---
+++ Code/test/servo_calib_test.py
@@ -0,0 +1,27 @@
+import pyfirmata
+
+scaling = 180/270
+offset = 135
+offsets = [x+offset for x in [2, 7, -4, -4]]
+
+board = pyfirmata.Arduino('/dev/ttyUSB0')
+
+# for servo in range(2,6):
+#     board.servo_config(servo,min_pulse=544,max_pulse=2400,angle=offset*scaling)
+
+l1 = board.get_pin('d:2:s')
+l1.write(offsets[0]*scaling)
+l2 = board.get_pin('d:3:s')
+l2.write(offsets[1]*scaling)
+r1 = board.get_pin('d:4:s')
+r1.write(offsets[2]*scaling)
+r2 = board.get_pin('d:5:s')
+r2.write(offsets[3]*scaling)
+
+servos = [l1, l2, r1, r2]
+
+while True:
+    i = int(input("Servo index:"))
+    angle = float(input("Angle:"))
+
+    servos[i].write((angle+offsets[i])*scaling)
