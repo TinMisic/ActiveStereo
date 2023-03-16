98b6fb95278a3b5bf599012471be160603b9cba5
Tin Misic <misic.tin@gmail.com>
2023-03-16 10:35:27 +0100

    Simple motion detection implementation. Used for initializing focus

---
+++ Code/motion_detection.py
@@ -0,0 +1,66 @@
+import cv2 as cv
+
+def centroidOfDif(frame1, frame2, mov_thresh):
+    difference = cv.absdiff(frame1, frame2)
+    threshold = cv.threshold(difference, 25, 255, cv.THRESH_BINARY)[1]
+
+    centroid = (None, None)    
+    if threshold.sum() > mov_thresh:
+        M = cv.moments(threshold)
+
+        cX = int(M['m10'] / M['m00'])
+        cY = int(M['m01'] / M['m00'])
+        centroid = (cX, cY)
+
+    return threshold, centroid
+
+def centroidSmoothing(oldCent, newCent, sFactor = 0.95):
+    if oldCent == (None, None):
+        return newCent
+    elif newCent == (None, None):
+        return oldCent
+    else:
+        x = int(oldCent[0] * sFactor + newCent[0] * (1-sFactor))
+        y = int(oldCent[1] * sFactor + newCent[1] * (1-sFactor))
+        return (x, y)
+
+if __name__=="__main__":
+    cap = cv.VideoCapture(4)
+
+    ret, start_frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        cap.release()
+        exit()
+
+    start_frame = cv.cvtColor(start_frame, cv.COLOR_RGB2GRAY)
+    start_frame = cv.GaussianBlur(start_frame, (5, 5), 0)
+    
+    old_centorid = (None, None)
+    while True:
+        ret, new_frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            cap.release()
+            exit()
+
+        new_frame = cv.cvtColor(new_frame, cv.COLOR_RGB2GRAY)
+        new_frame = cv.GaussianBlur(new_frame, (5, 5), 0)
+
+        threshold, centroid = centroidOfDif(start_frame, new_frame, 10000)
+        threshold = cv.cvtColor(threshold, cv.COLOR_GRAY2RGB)
+        centroid = centroidSmoothing(old_centorid, centroid, 0.85)
+        print(centroid)
+        if centroid != (None, None):
+            cv.circle(threshold, centroid, 10, color=(0, 0, 255), thickness=-1)
+            cv.putText(threshold, "target", (centroid[0] - 25, centroid[1] - 25),cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+        
+        start_frame = new_frame
+        old_centorid = centroid
+        cv.imshow("motion",threshold)
+        if cv.waitKey(1) == ord('q'):
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+
---
+++ Code/test/two_cams_test3.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import threading
+from queue import Queue
+
+endflag = False
+
+class CameraThread(threading.Thread):
+    def __init__(self, threadID, name, cameraID, queue):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.camera = cv2.VideoCapture(cameraID)
+        self.queue = queue
+
+    def run(self):
+        print("Starting " + self.name)
+        while True:
+            ret, frame = self.camera.read()
+            if not ret or endflag:
+                break
+            self.queue.put((self.threadID, frame))
+        self.camera.release()
+
+def main():
+    global endflag
+    # Create a queue to hold the frames from the camera threads
+    queue = Queue()
+
+    # Create two threads for two cameras
+    thread1 = CameraThread(1, "Camera 1", 6, queue)
+    thread2 = CameraThread(2, "Camera 2", 4, queue)
+
+    # Start the threads
+    thread1.start()
+    thread2.start()
+
+    # Create a window to display the combined frames
+    cv2.namedWindow("Combined Frames", cv2.WINDOW_NORMAL)
+
+    # Initialize the combined frames as an empty numpy array
+    left = np.zeros((480,640,3))
+    right = np.zeros((480,640,3))
+    combined = np.zeros((480,640,3))
+
+    # Loop to display the frames from the queue
+    while True:
+        # Get a frame from the queue
+        item = queue.get()
+        threadID, frame = item
+
+        # Display the frame in the appropriate window
+        #cv2.imshow("Camera " + str(threadID), frame)
+
+        # Combine the frames and display in the combined frames window
+        if threadID == 1:
+            left = frame
+        else:
+            right = frame
+            combined = np.hstack([left, right])
+            cv2.imshow("Combined Frames", combined)
+
+        # Exit if 'q' is pressed
+        if cv2.waitKey(1) == ord('q'):
+            endflag = True
+            break
+
+    # Wait for the threads to finish
+    thread1.join()
+    thread2.join()
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
